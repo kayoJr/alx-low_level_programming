46560ade89465b725a8dd81ff74506d206420741
kayoJr <kalebmelaku97@gmail.com>
2022-03-11 15:50:27 +0300

    first commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	int i, y;
-	
+
 	for (i = 0; i < 10; i++)
 	{
-		putchar (i + '0');
+		putchar(i + '0');
 	}
 	for (y = 'a'; y <= 'f'; y++)
 	{
-		putchar (y);
+		putchar(y);
 	}
 
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
