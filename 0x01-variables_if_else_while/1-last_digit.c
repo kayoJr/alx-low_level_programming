ed53967a30e434f6ce0113ace51c22b8eedc00f5
kayoJr <kalebmelaku97@gmail.com>
2022-03-11 15:32:30 +0300

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, s);
 	}
-	if (s < 6 && s!=0)
+	if (s < 6 && s != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
 	}
