6f92777aa30f028b18937956b45c7993590445ce
kayoJr <kalebmelaku97@gmail.com>
2022-04-04 13:17:04 +0300

    first commmit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,17 +8,12 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *p;
-	unsigned int i;
+char *m = malloc(size);
+if (size == 0 || m == 0)
+	return (0);
 
-	if (size == 0)
-	return (0);
-	p = malloc((size) * sizeof(char));
-	if (p == '\0')
-	return (0);
-	for (i = 0; i < size; i++)
-	{
-		p[i] = c;
-	}
-return (p);
+while (size--)
+	m[size] = c;
+
+return (m);
 }
