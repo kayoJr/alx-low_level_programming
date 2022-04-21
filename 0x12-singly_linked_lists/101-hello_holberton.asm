5259cccd83db7ece71afe08acf0ecb073ef6f14b
kayoJr <kalebmelaku97@gmail.com>
2022-04-21 23:45:17 +0300

    first commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,9 @@
-#include <stdio.h>
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("(Hello, Holberton.)\n");
-	return (0);
-}
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
